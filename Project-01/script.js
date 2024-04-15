b929f2c078deaee9d9098a0ad8eb846277abc66e
technicalmastersp <takebestservices@gmail.com>
2024-04-15 08:25:09 +0530

    Add sume new features ( Project : Video Player 4x )

--- Project-01/script.js
+++ Project-01/script.js
@@ -1,46 +1,48 @@
-function loadVideo(input) {
-    const video = document.getElementById('myVideo');
-    const file = input.files[0];
-    const url = URL.createObjectURL(file);
-    video.src = url;
-    video.style.display = "block";
-    input.style.display = "none";
-    
-    video.play();
-}
-function showDiv() {
-    const buttonBox = document.getElementById('buttonBox');
-    if (buttonBox.style.display === 'none') {
-        buttonBox.style.display = 'inline';
-    }   else {
-        buttonBox.style.display = 'none';
-    }
 
-    buttonBox.style.display = 'flex';
-    buttonBox.style.justifyContent = "center";
-    buttonBox.style.alignItems = "center";
+// ~~~~~~~~~~~~~~~~~~~~    for Video tag show or input tag hide ~~~~~~~~~~~~~~~~~~~~
+
+function loadVideo(input) {
+  const video = document.getElementById("myVideo");
+  const file = input.files[0];
+  const url = URL.createObjectURL(file);
+  video.src = url;
+  video.style.display = "block";
+  input.style.display = "none";
+  video.play();
 }
+
+// ~~~~~~~~~~~~~~~~~~~~    for Video speed button show or hide ~~~~~~~~~~~~~~~~~~~~
+
+var toggleButton = document.getElementById("toggleButton");
+var buttonBox = document.getElementById("buttonBox");
+
+toggleButton.addEventListener("click", function() {
+    if (buttonBox.style.display === "none") {
+        buttonBox.style.display = "block";
+    } else {
+        buttonBox.style.display = "none";
+    }
+});
+
 
 // ~~~~~~~~~~~~~~~~~~~~    for ( Change Video Speed ) Button ~~~~~~~~~~~~~~~~~~~~
 function setSpeed(speed) {
-    document.getElementById('myVideo').playbackRate = speed;
-};
-
+  document.getElementById("myVideo").playbackRate = speed;
+}
 
 // ~~~~~~~~~~~~~~~~~~~~    for ( Change Video ) Button ~~~~~~~~~~~~~~~~~~~~
-var videoInput = document.getElementById('videoInput');
+var videoInput = document.getElementById("videoInput");
 function changeVideoSource() {
-    videoInput.value = null;
-    videoInput.click();
+  videoInput.value = null;
+  videoInput.click();
 }
-
 
 // ~~~~~~~~~~~~~~~~~~~~    for ( Video Name Display ) Button ~~~~~~~~~~~~~~~~~~~~
-var videoTitleDiv = document.getElementById('videoTitle');
+var videoTitleDiv = document.getElementById("videoTitle");
 function setVideoTitle() {
-    var fileName = videoInput.files[0].name;
-    videoTitleDiv.textContent = "Currently Playing: " + fileName;
+  var fileName = videoInput.files[0].name;
+  videoTitleDiv.textContent = " " + fileName;
 }
-myVideo.addEventListener('loadedmetadata', function() {
-    setVideoTitle();
+myVideo.addEventListener("loadedmetadata", function () {
+  setVideoTitle();
 });
