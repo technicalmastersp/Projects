36dd7e75261ed95eee24686f134ddc7bafb97c18
technicalmastersp <takebestservices@gmail.com>
2024-08-07 18:16:50 +0530

    Project main version is final.

--- Love_Calculator/script/resultPage.js
+++ Love_Calculator/script/resultPage.js
@@ -8,63 +8,80 @@ function per(){
     a = Math.floor(Math.random()*100);
     return a;
 }
+const num = per();  // Only one time call random no.
 
 
-// ----------------------------------------  For create a message on random no. ----------------------------------------
+// ----------------------------------------  For sent our random no. to html page  ----------------------------------------
 
-document.getElementById('perBox').innerText = `💞 ${per()} % 💞`
+document.getElementById('perBox').innerText = `💞 Love : ${num} % 💞`;
 
-// ----------------------------------------  For create a message on random no. ----------------------------------------
+
+// ----------------------------------------  For create a message on random no.  ----------------------------------------
 
 function sp(){
-    const num = per()
     if(num >= 0 && num <= 12){
-        document.write("Good ", num , " (num >= 0 && num <= 12)")
+        return document.getElementById('randomNo').innerText = 'Good';
     }else{
         if(num >= 13 && num <= 29){
-            document.write("Nice ", num , " (num >= 13 && num <= 29")
+            return document.getElementById('randomNo').innerText = 'Nice';
         } else{
             if(num >= 30 && num <= 46){
-                document.write("Wow a good Connection ", num , " (num >= 30 && num <= 46)")
+                return document.getElementById('randomNo').innerText = 'Wow a good Connection';
             } else{
                 if(num >= 47 && num <= 73){
-                    document.write("Best Forewer ", num , " (num >= 47 && num <= 73)")
+                    return document.getElementById('randomNo').innerText = 'Best Forewer';
                 } else{
                     if(num >= 74 && num <= 91){
-                        document.write("Wonderfull Mach ", num , " (num >= 74 && num <= 91)")
+                        return document.getElementById('randomNo').innerText = 'Wonderfull Mach';
                     } else{
                         if(num >= 92 && num <= 100){
-                            document.write("💞❣️❤️ You Both are like a Soulmet ❤️❣️💞 ", num , " (num >= 92 && num <= 100)")
+                            return document.getElementById('randomNo').innerText = '💞❣️❤️ You Both are like a Soulmet ❤️❣️💞';
                         }
                     }
                 }
             }
         }
     }
-
-
-
-    // if(num >= 0 && num <= 12){
-    //     document.getElementById('randomNo').value = '("Good ", num , " (num >= 0 && num <= 12)")'
-    // }else{
-    //     if(num >= 13 && num <= 29){
-    //         document.getElementById('randomNo').value = '("Nice ", num , " (num >= 13 && num <= 29")'
-    //     } else{
-    //         if(num >= 30 && num <= 46){
-    //             document.getElementById('randomNo').value = '("Wow a good Connection ", num , " (num >= 30 && num <= 46)")'
-    //         } else{
-    //             if(num >= 47 && num <= 73){
-    //                 document.getElementById('randomNo').value = '("Best Forewer ", num , " (num >= 47 && num <= 73)")'
-    //             } else{
-    //                 if(num >= 74 && num <= 91){
-    //                     document.getElementById('randomNo').value = '("Wonderfull Mach ", num , " (num >= 74 && num <= 91)")'
-    //                 } else{
-    //                     if(num >= 92 && num <= 100){
-    //                         document.getElementById('randomNo').value = '("💞❣️❤️ You Both are like a Soulmet ❤️❣️💞 ", num , " (num >= 92 && num <= 100)")'
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
 }
+
+document.getElementById('randomNo').innerText = `💞 ${sp()} 💞`;
+
+
+// ----------------------------------------  For change photo on every message on random no.  ----------------------------------------
+
+let img = document.createElement('img');
+
+function changeImg(){
+    if(num >= 0 && num <= 12){
+        return img.src = '../Assets/image/GIF/1.gif';
+    }else{
+        if(num >= 13 && num <= 29){
+            return img.src = '../Assets/image/gif/2.gif';
+        } else{
+            if(num >= 30 && num <= 46){
+                return img.src = '../Assets/image/gif/7.gif';        // 3
+            } else{
+                if(num >= 47 && num <= 73){
+                    return img.src = '../Assets/image/gif/4.gif';
+                } else{
+                    if(num >= 74 && num <= 91){
+                        return img.src = '../Assets/image/gif/2.gif';    // 5, 8
+                    } else{
+                        if(num >= 92 && num <= 100){
+                            return img.src = '../Assets/image/gif/6.gif';
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+img.src = changeImg();
+img.height = 350;
+
+const div = document.getElementById('image')
+div.appendChild(img)
+
+
+// ========================================  End JavaScript Code  =================================================
--- Love_Calculator/script/script.js
+++ Love_Calculator/script/script.js
@@ -1,7 +1,62 @@
+// ========================================  Start JavaScript Code  =================================================
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  Start Version 01 Code  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// ----------------------------------------  For Welcome Alert and get Names  ----------------------------------------
+
 function verWelcome(){
     alert("Welcome to New and Simple Version of Love Calculator...")
     let fName = prompt("Type your Name : 👇👇")
     let sName = prompt("Type your Partner Name : 👇👇")
     confirm("Are You Excited...💞❣️💞")
 }
- 
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  End Version 01 Code  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+//                                                ******* 
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  Start Main Version Code  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// ----------------------------------------  Check Input field blank or filled  ----------------------------------------
+
+function checkInput() {
+    const name1 = document.getElementById('fNameInput').value.trim();
+    const name2 = document.getElementById('sNameInput').value.trim();
+
+    if (name1 === '') {
+        alert('❣️ Type Your Name ❣️');
+        return;
+    } else {
+        if (name2 === '') {
+            alert('❣️ Type Your Partner Name ❣️');
+            return;
+        }
+    }
+}
+
+
+// ----------------------------------------  For change heart color of main page after fill first name  ----------------------------------------
+
+function changeColor() {
+    const name01 = document.getElementById('fNameInput').value;
+    const name02 = document.getElementById('sNameInput').value;
+    const image = document.getElementById('heart')
+
+    if (name01 === '') {
+        image.style.filter = 'hue-rotate(0deg)'
+        return;
+    } else {
+        if (name02 === '') {
+            image.style.filter = 'hue-rotate(280deg)'
+            return;
+        }
+    }
+}
+
+
+// ========================================  End JavaScript Code  =================================================
